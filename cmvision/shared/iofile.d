06964c0f4cea1ec04a9f34cbf81dc1f6d6d5ffe0
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-07-15 00:56:22 +0300

    Updated file modes.

---
+++ cmvision/shared/iofile.d
@@ -0,0 +1,91 @@
+shared/iofile.o :  shared/iofile.cc /usr/include/stdc-predef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.9/include/stdarg.h /usr/include/stdio.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.9/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio2.h shared/iofile.h \
+ /usr/include/string.h /usr/include/xlocale.h \
+ /usr/include/x86_64-linux-gnu/bits/string3.h /usr/include/zlib.h \
+ /usr/include/x86_64-linux-gnu/zconf.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.9/include-fixed/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/4.9/include-fixed/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h /usr/include/time.h \
+ /usr/include/endian.h /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/sigset.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/bits/select2.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h /usr/include/unistd.h \
+ /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
+ /usr/include/x86_64-linux-gnu/bits/environments.h \
+ /usr/include/x86_64-linux-gnu/bits/confname.h /usr/include/getopt.h \
+ /usr/include/x86_64-linux-gnu/bits/unistd.h \
+ /usr/include/x86_64-linux-gnu/sys/stat.h \
+ /usr/include/x86_64-linux-gnu/bits/stat.h /usr/include/fcntl.h \
+ /usr/include/x86_64-linux-gnu/bits/fcntl.h \
+ /usr/include/x86_64-linux-gnu/bits/fcntl-linux.h \
+ /usr/include/x86_64-linux-gnu/bits/uio.h \
+ /usr/include/x86_64-linux-gnu/bits/fcntl2.h
+
+shared/iofile.d :  $(wildcard shared/iofile.cc) $(wildcard /usr/include/stdc-predef.h) \
+ $(wildcard /usr/lib/gcc/x86_64-linux-gnu/4.9/include/stdarg.h) $(wildcard /usr/include/stdio.h) \
+ $(wildcard /usr/include/features.h) $(wildcard /usr/include/x86_64-linux-gnu/sys/cdefs.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/wordsize.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/gnu/stubs.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/gnu/stubs-64.h) \
+ $(wildcard /usr/lib/gcc/x86_64-linux-gnu/4.9/include/stddef.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/types.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/typesizes.h) $(wildcard /usr/include/libio.h) \
+ $(wildcard /usr/include/_G_config.h) $(wildcard /usr/include/wchar.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/stdio_lim.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/sys_errlist.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/stdio.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/stdio2.h) $(wildcard shared/iofile.h) \
+ $(wildcard /usr/include/string.h) $(wildcard /usr/include/xlocale.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/string3.h) $(wildcard /usr/include/zlib.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/zconf.h) \
+ $(wildcard /usr/lib/gcc/x86_64-linux-gnu/4.9/include-fixed/limits.h) \
+ $(wildcard /usr/lib/gcc/x86_64-linux-gnu/4.9/include-fixed/syslimits.h) \
+ $(wildcard /usr/include/limits.h) $(wildcard /usr/include/x86_64-linux-gnu/bits/posix1_lim.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/local_lim.h) \
+ $(wildcard /usr/include/linux/limits.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/posix2_lim.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/xopen_lim.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/sys/types.h) $(wildcard /usr/include/time.h) \
+ $(wildcard /usr/include/endian.h) $(wildcard /usr/include/x86_64-linux-gnu/bits/endian.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/byteswap.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/byteswap-16.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/sys/select.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/select.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/sigset.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/time.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/select2.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/sys/sysmacros.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h) $(wildcard /usr/include/unistd.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/posix_opt.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/environments.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/confname.h) $(wildcard /usr/include/getopt.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/unistd.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/sys/stat.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/stat.h) $(wildcard /usr/include/fcntl.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/fcntl.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/fcntl-linux.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/uio.h) \
+ $(wildcard /usr/include/x86_64-linux-gnu/bits/fcntl2.h)
