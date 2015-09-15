6f00686647a67195c33c85dcb3bf57a1aeefab26
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-09-15 11:59:11 +0300

    Mathematics: Initialised rectangle class. #41

---
+++ src/modules/mathematics/rectangle.cpp
@@ -0,0 +1,49 @@
+/**
+ * Tuum
+ * Robotex 2015
+ * University of Tartu
+ * Team:
+ *   Kristjan Kanarbik
+ *   Meelik Kiik
+ *   Ants-Oskar Mäesalu
+ *   Mart Müllerbeck
+ *   Kaur Viiklaid
+ *   Indrek Virro
+ *   Mihkel Väljaots
+
+ * Rectangle class
+ * Contributors:
+ *   Ants-Oskar Mäesalu
+ */
+
+#include "rectangle.h"
+
+Rectangle::Rectangle(const Rectangle& other):
+x{other.getX()},
+y{other.getY()}
+{}
+
+Rectangle::Rectangle(const double& x, const double& y):
+x{x},
+y{y}
+{}
+
+void Rectangle::setX(const double& x) {
+  this->x = x;
+}
+
+void Rectangle::setY(const double& y) {
+  this->y = y;
+}
+
+double Rectangle::getSize() const {
+  return x * y;
+}
+
+double Rectangle::getX() const {
+  return x;
+}
+
+double Rectangle::getY() const {
+  return y;
+}
---
+++ src/modules/mathematics/rectangle.h
@@ -0,0 +1,36 @@
+/**
+ * Tuum
+ * Robotex 2015
+ * University of Tartu
+ * Team:
+ *   Kristjan Kanarbik
+ *   Meelik Kiik
+ *   Ants-Oskar Mäesalu
+ *   Mart Müllerbeck
+ *   Kaur Viiklaid
+ *   Indrek Virro
+ *   Mihkel Väljaots
+
+ * Rectangle class
+ * Contributors:
+ *   Ants-Oskar Mäesalu
+ */
+
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+class Rectangle {
+private:
+  double x;
+  double y;
+public:
+  Rectangle(const Rectangle&);
+  Rectangle(const double&, const double&);
+  void setX(const double&);
+  void setY(const double&);
+  double getSize() const;
+  double getX() const;
+  double getY() const;
+};
+
+#endif // RECTANGLE_H
