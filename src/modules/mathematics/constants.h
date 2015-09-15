6cb4034020728cef77bdea2012359f63f5709bb9
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-09-15 12:07:05 +0300

    Mathematics: Initialised mathematical constants header. #41

---
+++ src/modules/mathematics/constants.h
@@ -0,0 +1,19 @@
+/**
+ * Tuum
+ * Robotex 2015
+ * University of Tartu
+ * Team:
+ *   Kristjan Kanarbik
+ *   Meelik Kiik
+ *   Ants-Oskar Mäesalu
+ *   Mart Müllerbeck
+ *   Kaur Viiklaid
+ *   Indrek Virro
+ *   Mihkel Väljaots
+
+ * Mathematical constants header
+ * Contributors:
+ *   Ants-Oskar Mäesalu
+ */
+
+#define PI 3.1415926536
