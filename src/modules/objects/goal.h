ece8e80c484a171e048ab584daeaacbc9f6242e3
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-09-16 03:17:10 +0300

    Objects: Added goal class. #40

---
+++ src/modules/objects/goal.h
@@ -0,0 +1,35 @@
+/**
+ * Tuum
+ * Robotex 2015
+ * University of Tartu
+ * Team:
+ *   Kristjan Kanarbik
+ *   Meelik Kiik
+ *   Ants-Oskar Mäesalu
+ *   Mart Müllerbeck
+ *   Kaur Viiklaid
+ *   Indrek Virro
+ *   Mihkel Väljaots
+
+ * Goal class
+ * Contributors:
+ *   Ants-Oskar Mäesalu
+ */
+
+#ifndef GOAL_H
+#define GOAL_H
+
+#include "../mathematics/rectangle.h"
+#include "../mathematics/point2d.h"
+
+class Goal: public Object, public Rectangle {
+private:
+  // TODO: Add color
+public:
+  Goal(const Goal&);
+  Goal(const Point2D*, const double&, const double&); // TODO: Add default goal dimensions
+  Goal(const double&, const double&, const double&, const double&); // TODO: Add default goal dimensions
+  void swapColor();
+}
+
+#endif // GOAL_H
