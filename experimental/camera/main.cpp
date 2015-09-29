4132f65a0fb55d56e75d0323b78ec16d5567e03d
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-09-29 13:58:23 +0300

    Camera: Initialised experimental camera test file and fixed elementary bugs. #7

---
+++ experimental/camera/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Tuum
+ * Robotex 2015
+ * University of Tartu
+ * Team:
+ *   Kristjan Kanarbik
+ *   Meelik Kiik
+ *   Ants-Oskar Mäesalu
+ *   Mart Müllerbeck
+ *   Kaur Viiklaid
+ *   Indrek Virro
+ *   Mihkel Väljaots
+
+ * Camera experimental main class
+ * Contributors:
+ *   Ants-Oskar Mäesalu
+ */
+
+#include "camera.h"
+
+int main() {
+  Camera *camera = new Camera();
+  return 0;
+}
