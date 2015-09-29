bcc5b363a6f3db83ebe8825cfc1847e95aaf8469
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-09-30 01:39:49 +0300

    Camera: Started commenting camera class. Implemented closing the camera device. #7

--- experimental/camera/camera.cpp
+++ experimental/camera/camera.cpp
@@ -1,28 +1,53 @@
 /**
- * Tuum
- * Robotex 2015
- * University of Tartu
- * Team:
- *   Kristjan Kanarbik
- *   Meelik Kiik
- *   Ants-Oskar Mäesalu
- *   Mart Müllerbeck
- *   Kaur Viiklaid
- *   Indrek Virro
- *   Mihkel Väljaots
+  Tuum
+  Robotex 2015
+  University of Tartu
+  Team:
+    Kristjan Kanarbik
+    Meelik Kiik
+    Ants-Oskar Mäesalu
+    Mart Müllerbeck
+    Kaur Viiklaid
+    Indrek Virro
+    Mihkel Väljaots
 
- * Camera class
- * Contributors:
- *   Ants-Oskar Mäesalu
- */
+  Camera class
+    Class for communication with the robot's cameras, based on the V4L2 API.
+    The video capture example provided with the V4L2 API has been used as a
+    model for the class.
+
+  Contributors:
+    Ants-Oskar Mäesalu
+
+  Copyright (c) 2015 Ants-Oskar Mäesalu
+*/
 
 #include "camera.h"
+
+#include "unistd.h"             // Operating system API header
+#include <linux/videodev2.h>    // V4L2 header
 
 Camera::Camera(const std::string &device, const int &width, const int &height):
 device{device},
 width{width},
 height{height}
-{}
+{
+  openDevice();
+}
+
+Camera::~Camera() {
+  closeDevice();
+}
+
+void Camera::openDevice() {
+  // TODO
+}
+
+void Camera::closeDevice() {
+  if (close(fileDescriptor) == -1)
+    throw runtime_error("Error upon closing device.");
+  fileDescriptor = -1;
+}
 
 std::string Camera::getDevice() const {
   return device;
--- experimental/camera/camera.h
+++ experimental/camera/camera.h
@@ -1,20 +1,26 @@
 /**
- * Tuum
- * Robotex 2015
- * University of Tartu
- * Team:
- *   Kristjan Kanarbik
- *   Meelik Kiik
- *   Ants-Oskar Mäesalu
- *   Mart Müllerbeck
- *   Kaur Viiklaid
- *   Indrek Virro
- *   Mihkel Väljaots
+  Tuum
+  Robotex 2015
+  University of Tartu
+  Team:
+    Kristjan Kanarbik
+    Meelik Kiik
+    Ants-Oskar Mäesalu
+    Mart Müllerbeck
+    Kaur Viiklaid
+    Indrek Virro
+    Mihkel Väljaots
 
- * Camera class
- * Contributors:
- *   Ants-Oskar Mäesalu
- */
+  Camera class
+    Class for communication with the robot's cameras, based on the V4L2 API.
+    The video capture example provided with the V4L2 API has been used as a
+    model for the class.
+
+  Contributors:
+    Ants-Oskar Mäesalu
+
+  Copyright (c) 2015 Ants-Oskar Mäesalu
+*/
 
 #ifndef CAMERA_H
 #define CAMERA_H
@@ -24,15 +30,58 @@
 #include "constants.h"
 
 class Camera {
+
 private:
   std::string device;
   int width;
   int height;
+
+  int fileDescriptor;
+
+  /**
+    // TODO
+  */
+  void openDevice();
+
+  /**
+    Closes the camera device. All I/O progress is terminated and resources
+    associated with the file descriptor are freed. However, data format
+    parameters, current input or output, control values or other properties
+    remain unchanged.
+  */
+  void closeDevice();
+
 public:
-  Camera(const std::string& = CAMERA_DEVICE, const int& = CAMERA_WIDTH, const int& = CAMERA_HEIGHT);
+
+  /**
+    Camera constructor. Receives the camera device location and the preferred
+    resolution dimensions as parameters.
+    Opens the device.
+  */
+  Camera(const std::string& = CAMERA_DEVICE, const int& = CAMERA_WIDTH,
+         const int& = CAMERA_HEIGHT);
+
+  /**
+    Camera destructor.
+    Closes the device.
+  */
+  ~Camera();
+
+  /**
+    Returns the camera's device path.
+  */
   std::string getDevice() const;
+
+  /**
+    Returns the camera's resolution width.
+  */
   int getWidth() const;
+
+  /**
+    Returns the camera's resolution height.
+  */
   int getHeight() const;
+
 };
 
 #endif // CAMERA_H
