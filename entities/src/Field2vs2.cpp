f2a48fd011984109b7882dd88be91c05427219a2
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-10-09 20:21:11 +0300

    Entities: Repositioned pointer and reference characters.

--- entities/src/Field2vs2.cpp
+++ entities/src/Field2vs2.cpp
@@ -11,7 +11,7 @@
 
 namespace rtx {
 
-  Field2vs2::Field2vs2(const Field2vs2& other):
+  Field2vs2::Field2vs2(const Field2vs2 &other):
     Field(other),
     ball{other.getBall()},
     ally{other.getAlly()},
@@ -20,7 +20,7 @@ namespace rtx {
 
   }
 
-  Field2vs2::Field2vs2(RobotSelf* robotSelf, const double& width,
+  Field2vs2::Field2vs2(RobotSelf *robotSelf, const double &width,
                        const double& length):
     Field(robotSelf, width, length)
   {
@@ -28,9 +28,9 @@ namespace rtx {
     // TODO: Initialise enemies and ball
   }
 
-  Field2vs2::Field2vs2(RobotSelf* robotSelf, RobotAlly* ally,
-                       RobotEnemy** enemies, const double& width,
-                       const double& length):
+  Field2vs2::Field2vs2(RobotSelf *robotSelf, RobotAlly *ally,
+                       RobotEnemy **enemies, const double &width,
+                       const double &length):
     Field(robotSelf, width, length),
     ally{ally}
   {
@@ -38,10 +38,10 @@ namespace rtx {
     // TODO: Initialise ball
   }
 
-  Field2vs2::Field2vs2(Goal* friendlyGoal, Goal* enemyGoal,
-                       RobotSelf* robotSelf, RobotAlly* ally,
-                       RobotEnemy** enemies, const double& width,
-                       const double& length):
+  Field2vs2::Field2vs2(Goal *friendlyGoal, Goal *enemyGoal,
+                       RobotSelf *robotSelf, RobotAlly *ally,
+                       RobotEnemy **enemies, const double &width,
+                       const double &length):
     Field(friendlyGoal, enemyGoal, robotSelf, width, length),
     ally{ally}
   {
@@ -49,9 +49,9 @@ namespace rtx {
     // TODO: Initialise ball
   }
 
-  Field2vs2::Field2vs2(RobotSelf* robotSelf, RobotAlly* ally,
-                       RobotEnemy** enemies, Ball* ball, const double& width,
-                       const double& length):
+  Field2vs2::Field2vs2(RobotSelf *robotSelf, RobotAlly *ally,
+                       RobotEnemy **enemies, Ball *ball, const double &width,
+                       const double &length):
     Field(robotSelf, width, length),
     ally{ally}
   {
@@ -59,10 +59,10 @@ namespace rtx {
     this->ball = ball; // Causes warning when in initialization list
   }
 
-  Field2vs2::Field2vs2(Goal* friendlyGoal, Goal* enemyGoal,
-                       RobotSelf* robotSelf, RobotAlly* ally,
-                       RobotEnemy** enemies, Ball* ball, const double& width,
-                       const double& length):
+  Field2vs2::Field2vs2(Goal *friendlyGoal, Goal *enemyGoal,
+                       RobotSelf *robotSelf, RobotAlly *ally,
+                       RobotEnemy **enemies, Ball *ball, const double &width,
+                       const double &length):
     Field(friendlyGoal, enemyGoal, robotSelf, width, length),
     ally{ally}
   {
