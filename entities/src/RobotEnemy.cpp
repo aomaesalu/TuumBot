6f5f6dfcdcd7c77007a96318e0ba331dade71a17
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-10-09 20:09:42 +0300

    Entities: Reimplemented robot enemy class.

---
+++ entities/src/RobotEnemy.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file RobotEnemy.cpp
+ * Enemy robot class.
+ *
+ * @authors Ants-Oskar Mäesalu
+ * @version 0.1
+ */
+
+#include "RobotEnemy.hpp"
+
+
+namespace rtx {
+
+  RobotEnemy::RobotEnemy(const RobotEnemy& other):
+    Robot(other.getPosition(), other.getRadius())
+  {
+
+  }
+
+  RobotEnemy::RobotEnemy(const Point2D* position, const double& radius):
+    Robot(position, radius)
+  {
+
+  }
+
+  RobotEnemy::RobotEnemy(const double& x, const double& y, const double& radius):
+    Robot(x, y, radius)
+  {
+    
+  }
+
+
+};
