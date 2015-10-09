4fc1436004e5ae4f1c9ed9c3c8ec92bcca7f3e98
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-10-09 21:08:12 +0300

    Applications: Fixed 1vs1 application main module to correspond with the updated project.

--- user/applications/competition/1vs1/main.cpp
+++ user/applications/competition/1vs1/main.cpp
@@ -1,31 +1,26 @@
 /**
- * Tuum
- * Robotex 2015
- * University of Tartu
- * Team:
- *   Kristjan Kanarbik
- *   Meelik Kiik
- *   Ants-Oskar Mäesalu
- *   Mart Müllerbeck
- *   Kaur Viiklaid
- *   Indrek Virro
- *   Mihkel Väljaots
-
- * 1vs1 competition program
- * Contributors:
- *   Ants-Oskar Mäesalu
-
- * main.cpp
- * The main function of the competition program.
+ * @file main.cpp
+ * Football 1vs1 application main module.
+ *
+ * @authors Ants-Oskar Mäesalu
+ * @version 0.1
  */
 
 #include <cstdlib>
-#include "../../../modules/objects/robotSelf.h"
-#include "../../../modules/objects/field1vs1.h"
+
+#include "RobotSelf.hpp"
+#include "Field1vs1.hpp"
+
+using namespace rtx;
+
 
 int main() {
+
 	// Initialise game objects
 	RobotSelf* self = new RobotSelf();
 	Field1vs1* field = new Field1vs1(self);
+
+	// TODO
+
 	return EXIT_SUCCESS;
 }
