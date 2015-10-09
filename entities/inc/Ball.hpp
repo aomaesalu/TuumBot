dd970b53841c89a26a3ccb6921092c61d606a2f4
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-10-09 18:19:25 +0300

    Entities: Reimplemented Ball class.

--- entities/inc/Ball.hpp
+++ entities/inc/Ball.hpp
@@ -1,16 +1,32 @@
+/**
+ * @file Ball.hpp
+ * Description placeholder.
+ *
+ * @authors Ants-Oskar Mäesalu
+ * @version 0.1
+ */
 
-#ifndef RTX_BALL_H
-#define RTX_BALL_H
+#ifndef BALL_H
+#define BALL_H
+
+
+#include "entityConstants.h"
+#include "Entity.h"
+#include "Circle.h"
+#include "Point2D.h"
 
 namespace rtx {
 
-  class Ball {
-    private:
-      int m_placeholder;
+  class Ball: public Entity, public Circle {
     public:
-      Ball();
-  };
+      Ball(const Ball&);
+      Ball(const Point2D*, const double& = BALL_RADIUS);
+      Ball(const double&, const double&, const double& = BALL_RADIUS);
 
-}
+    private:
+      // TODO
+  }
 
-#endif
+};
+
+#endif // BALL_H
