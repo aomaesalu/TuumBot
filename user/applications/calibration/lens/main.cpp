e8fbe7b84f9e96a298735e206d94a346da625399
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-10-09 21:19:11 +0300

    Applications: Fixed calibration and simulation applications to correspond with the updated project.

--- user/applications/calibration/lens/main.cpp
+++ user/applications/calibration/lens/main.cpp
@@ -1,27 +1,19 @@
 /**
- * Tuum
- * Robotex 2015
- * University of Tartu
- * Team:
- *   Kristjan Kanarbik
- *   Meelik Kiik
- *   Ants-Oskar Mäesalu
- *   Mart Müllerbeck
- *   Kaur Viiklaid
- *   Indrek Virro
- *   Mihkel Väljaots
-
- * Lens calibration program
- * Contributors:
- *   Ants-Oskar Mäesalu
-
- * main.cpp
- * The main function of the color calibration program.
+ * @file main.cpp
+ * Lens calibration application main module.
+ *
+ * @authors Ants-Oskar Mäesalu
+ * @version 0.1
  */
 
 #include <cstdlib>
 
+//using namespace rtx;
+
+
 int main() {
+
+	// TODO
 
 	return EXIT_SUCCESS;
 }
