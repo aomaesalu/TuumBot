b96065e8055bf855db6f86eb9885ea2d52db5d9f
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-10-09 19:50:31 +0300

    Entities: Reimplemented robot self class.

---
+++ entities/src/RobotSelf.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file RobotSelf.cpp
+ * Robot self class.
+ *
+ * @authors Ants-Oskar Mäesalu
+ * @version 0.1
+ */
+
+#include "RobotSelf.hpp"
+
+
+namespace rtx {
+
+  RobotSelf::RobotSelf(const RobotSelf& other):
+    Robot(other.getPosition(), other.getRadius())
+  {
+
+  }
+
+  RobotSelf::RobotSelf(const Point2D* position, const double& radius):
+    Robot(position, radius)
+  {
+
+  }
+
+  RobotSelf::RobotSelf(const double& x, const double& y, const double& radius):
+    Robot(x, y, radius)
+  {
+    
+  }
+
+
+};
