6fc4480b2c5872fcdbccdd2613aa64e9649ba79c
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-10-09 18:19:25 +0300

    Entities: Reimplemented Ball class.

---
+++ entities/inc/Ball.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file Ball.hpp
+ * Description placeholder.
+ *
+ * @authors Ants-Oskar Mäesalu
+ * @version 0.1
+ */
+
+#ifndef BALL_H
+#define BALL_H
+
+
+#include "entityConstants.h"
+#include "Entity.h"
+#include "Circle.h"
+#include "Point2D.h"
+
+namespace rtx {
+
+  class Ball: public Entity, public Circle {
+    public:
+      Ball(const Ball&);
+      Ball(const Point2D*, const double& = BALL_RADIUS);
+      Ball(const double&, const double&, const double& = BALL_RADIUS);
+
+    private:
+      // TODO
+  }
+
+};
+
+#endif // BALL_H
---
+++ entities/src/Ball.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file Ball.hpp
+ * Description placeholder.
+ *
+ * @authors Ants-Oskar Mäesalu
+ * @version 0.1
+ */
+
+#include "Ball.hpp"
+
+
+namespace rtx {
+
+  Ball::Ball(const Ball &other):
+    Entity(other.getPosition()),
+    Circle(other.getRadius())
+  {
+
+  }
+
+  Ball::Ball(const Point2D *position, const double &radius):
+    Entity(position),
+    Circle(radius)
+  {
+
+  }
+
+  Ball::Ball(const double &x, const double &y, const double &radius):
+    Entity(new Point2D(x, y)),
+    Circle(radius)
+  {
+    
+  }
+
+};
