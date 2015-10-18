9aaea8455932ea80a16219ee5468023f3d6bd658
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-10-18 15:37:48 +0300

    Experimental: Initialised GUI. #46

---
+++ experimental/gui/main.cpp
@@ -0,0 +1,27 @@
+/**
+  Tuum
+  Robotex 2015
+  University of Tartu
+  Team:
+    Kristjan Kanarbik
+    Meelik Kiik
+    Ants-Oskar Mäesalu
+    Mart Müllerbeck
+    Kaur Viiklaid
+    Indrek Virro
+    Mihkel Väljaots
+
+  Experimental GUI class main file
+
+  Contributors:
+    Ants-Oskar Mäesalu
+
+  Copyright (c) 2015 Ants-Oskar Mäesalu
+*/
+
+#include "gui.h"
+
+int main() {
+  GUI *window = new GUI();
+  return 0;
+}
