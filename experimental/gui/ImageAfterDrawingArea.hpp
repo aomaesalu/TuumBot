e58dcfbbd1dbf4f637b04a29139d5cd0f4caeb27
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-11-01 18:58:53 +0200

    Experimental color calibration: Removed dead code. #21, #53, #58

--- experimental/gui/ImageAfterDrawingArea.hpp
+++ experimental/gui/ImageAfterDrawingArea.hpp
@@ -11,7 +11,6 @@
 
 #include <vector>
 #include <set>
-#include <map>
 
 #include <gtkmm.h>
 #include <gdkmm/pixbuf.h>
@@ -38,34 +37,17 @@ class ImageAfterDrawingArea: public ImageDrawingArea {
 
     Gtk::Scale *deltaScale;
 
-    //std::vector<std::map<unsigned int, std::map<unsigned int, std::map<unsigned int, bool>>>> filter;
-    //std::vector<std::map<unsigned int, std::map<unsigned int, std::map<unsigned int, bool>>>> filterAdditionBuffer;
-    //std::vector<std::map<unsigned int, std::map<unsigned int, std::map<unsigned int, bool>>>> filterRemovalBuffer;
     std::vector<std::set<unsigned int>> filter;
     std::vector<std::set<unsigned int>> filterAdditionBufferList;
     std::vector<std::set<unsigned int>> filterRemovalBufferList;
-
-    std::map<unsigned int, std::map<unsigned int, std::map<unsigned int, bool>>> resettedFilter;
 
     void initialiseProperties();
     void initialiseImage();
     void initialiseDeltaScale(Gtk::Scale*);
     void initialiseFilters();
 
-    //void initialiseSingleFilterMap(std::map<unsigned int, std::map<unsigned int, std::map<unsigned int, bool>>>&);
-    //void initialiseFilterMap(std::vector<std::map<unsigned int, std::map<unsigned int, std::map<unsigned int, bool>>>>&);
-    //void initialiseFilter();
-    //void initialiseFilterBuffers();
-    //void initialiseFilterAdditionBuffer();
-    //void initialiseFilterRemovalBuffer();
-    //void initialiseResettedFilter();
-
-    //void resetSingleFilterMap(std::map<unsigned int, std::map<unsigned int, std::map<unsigned int, bool>>>&);
-    //void resetFilterMap(std::vector<std::map<unsigned int, std::map<unsigned int, std::map<unsigned int, bool>>>>&);
     void resetFilter();
     void resetFilterBuffers();
-    //void resetFilterAdditionBuffer();
-    //void resetFilterRemovalBuffer();
 
     bool applyFilter();
     bool drawImage(const Cairo::RefPtr<Cairo::Context>&);
