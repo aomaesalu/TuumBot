6079aa4c924a65ca4cf09a210eedef05b8fb514d
Meelik Kiik <kiik.meelik@gmail.com>
2015-11-02 19:12:18 +0200

    Update default application code.

--- user/inc/application.h
+++
@@ -1,9 +0,0 @@
-#ifndef APPLICATION_H_
-#define APPLICATION_H_
-
-//#include "tuum_vision.hpp"
-
-#include "rtxvision.h"
-#include "rtxgeom.h"
-
-#endif /* APPLICATION_H_ */
---
+++ user/inc/application.hpp
@@ -0,0 +1,9 @@
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+#include "rtxhal.hpp"
+
+#include "tuum_visioning.hpp"
+#include "tuum_localization.hpp"
+
+#endif /* APPLICATION_H_ */
