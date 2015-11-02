a03f03d94eabed818962aad8434246ee67f444a0
Meelik Kiik <kiik.meelik@gmail.com>
2015-11-02 19:12:18 +0200

    Update default application code.

--- user/src/application.cpp
+++ user/src/application.cpp
@@ -1,10 +1,25 @@
-#include "application.h"
+#include "application.hpp"
 #include <iostream>
 
 using namespace std;
+using namespace rtx;
 
 int main() {
   printf("main(): Default user application.\n");
+
+  // Initialize hardware
+  rtx::hal::setup();
+
+  // Initialize system modules
+  Visioning::setup();
+  Localization::setup();
+
+  bool running = true;
+  while(running) {
+    rtx::hal::process();
+
+    Visioning::process();
+    Localization::process();
+  }
   return 0;
 }
-
