bb3049ec8b21979176b4211886d35f60cfb88bc4
Meelik Kiik <kiik.meelik@gmail.com>
2015-11-03 18:08:55 +0200

    Use standard 'gnu++11' instead.

--- geometry/src/build.mk
+++ geometry/src/build.mk
@@ -15,7 +15,7 @@ CPPSRC += $(call target_files,src/,*.cpp)
 # ASM source files included in this build.
 ASRC +=
 
-CPPFLAGS += -std=gnu++14
+CPPFLAGS += -std=gnu++11
 
 BUILTINS_EXCLUDE =
 CFLAGS += $(addprefix -fno-builtin-,$(BUILTINS_EXCLUDE))
