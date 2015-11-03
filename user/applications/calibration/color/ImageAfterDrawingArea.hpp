ea0a63df3cfeea1598947a9493b75ecf454d0ee0
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-11-03 20:24:23 +0200

    Project: Moved experimental color calibration application code to the appropriate place. #62

--- user/applications/calibration/color/ImageAfterDrawingArea.hpp
+++ user/applications/calibration/color/ImageAfterDrawingArea.hpp
@@ -1,6 +1,6 @@
 /**
  * @file ImageDrawingArea.hpp
- * Experimental GUI color calibration application "after" image drawing area.
+ * Color calibration application "after" image drawing area.
  *
  * @authors Ants-Oskar Mäesalu
  * @version 0.1
