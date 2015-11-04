51707ac73b5e4ba46cb5ef8665bb6333e604f43c
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-11-04 02:19:43 +0200

    Vision: Moved the V4L2 camera interface into the rtx namespace. #63

--- hal/src/phobos/V4L2Camera.hpp
+++ hal/src/phobos/V4L2Camera.hpp
@@ -17,175 +17,179 @@
 #include "cameraConstants.h"    // Camera constants
 
 
-/**
-  Camera image buffer structure.
-*/
-struct buffer {
-  void *data;
-  size_t size;
-};
-
-struct Frame {
-  unsigned char   *data;
-  size_t          width;
-  size_t          height;
-  size_t          size;     // width * height * 3
-};
-
-class Camera {
-
-public:
+namespace rtx {
 
   /**
-    Camera constructor. Receives the camera device location and the preferred
-    resolution dimensions as parameters.
-    Opens and initialises the device, and starts capturing.
+    Camera image buffer structure.
   */
-  Camera(const std::string& = CAMERA_DEVICE, const int& = CAMERA_WIDTH,
-         const int& = CAMERA_HEIGHT);
+  struct buffer {
+    void *data;
+    size_t size;
+  };
 
-  /**
-    Camera destructor.
-    Stops capturing, uninitialises and closes the device.
-  */
-  ~Camera();
+  struct Frame {
+    unsigned char   *data;
+    size_t          width;
+    size_t          height;
+    size_t          size;     // width * height * 3
+  };
 
-  /**
-    Returns the camera device's path.
-  */
-  std::string getDevice() const;
+  class Camera {
 
-  /**
-    Returns the camera resolution's width.
-  */
-  size_t getWidth() const;
+  public:
 
-  /**
-    Returns the camera resolution's height.
-  */
-  size_t getHeight() const;
+    /**
+      Camera constructor. Receives the camera device location and the preferred
+      resolution dimensions as parameters.
+      Opens and initialises the device, and starts capturing.
+    */
+    Camera(const std::string& = CAMERA_DEVICE, const int& = CAMERA_WIDTH,
+           const int& = CAMERA_HEIGHT);
 
-  const Frame& getFrame(unsigned int = 1);
+    /**
+      Camera destructor.
+      Stops capturing, uninitialises and closes the device.
+    */
+    ~Camera();
 
-private:
+    /**
+      Returns the camera device's path.
+    */
+    std::string getDevice() const;
 
-  /**
-    The camera device's path. The default value for this variable is described
-    in the camera constants file. The value actually used in the program can be
-    set upon class initialisation.
-    In Linux operating systems, this is usually /dev/videoN, where N is the
-    video device's number.
-  */
-  std::string device;
+    /**
+      Returns the camera resolution's width.
+    */
+    size_t getWidth() const;
 
-  /**
-    The camera resolution's width. The default value for this variable is
-    described in the camera constants file. The value actually used in the
-    program can be set upon class initialisation.
-  */
-  size_t width;
+    /**
+      Returns the camera resolution's height.
+    */
+    size_t getHeight() const;
 
-  /**
-    The camera resolution's height. The default value for this variable is
-    described in the camera constants file. The value actually used in the
-    program can be set upon class initialisation.
-  */
-  size_t height;
+    const Frame& getFrame(unsigned int = 1);
 
-  /**
-    The camera device's file descriptor. This specific value is set upon opening
-    the device and is used in I/O operations performed on the device, and upon
-    closing the device.
-  */
-  int fileDescriptor;
+  private:
 
-  struct buffer *buffers;
-  unsigned int numberOfBuffers;
-  Frame frame;
-  size_t stride;
+    /**
+      The camera device's path. The default value for this variable is described
+      in the camera constants file. The value actually used in the program can be
+      set upon class initialisation.
+      In Linux operating systems, this is usually /dev/videoN, where N is the
+      video device's number.
+    */
+    std::string device;
 
-  /**
-    Opens the camera device. Establishes a connection between a file and a file
-    descriptor. Creates an open file description that refers to a file and a
-    file descriptor that refers to that open file description. The file
-    descriptor is used by other I/O functions to refer to that file.
-    The file descriptor is set to the lowest file descriptor not currently open
-    for the current process. The open file description is new, and therefore the
-    file descriptor shall not share it with any other process in the system.
-    Throws a runtime error if the device cannot be identified, if it is not an
-    appropriate device, or if it cannot be opened.
-  */
-  void openDevice();
+    /**
+      The camera resolution's width. The default value for this variable is
+      described in the camera constants file. The value actually used in the
+      program can be set upon class initialisation.
+    */
+    size_t width;
 
-  /**
-    Closes the camera device. All I/O progress is terminated and resources
-    associated with the file descriptor are freed. However, data format
-    parameters, current input or output, control values or other properties
-    remain unchanged.
-    Throws a runtime error if the device cannot be closed. This can only happen
-    if the file descriptor used upon closing the device is not a valid open file
-    descriptor of the device.
-  */
-  void closeDevice();
+    /**
+      The camera resolution's height. The default value for this variable is
+      described in the camera constants file. The value actually used in the
+      program can be set upon class initialisation.
+    */
+    size_t height;
 
-  /**
-    Initialises the camera device.
-    Checks the camera device's V4L2 capabilites, cropping capabilities, [...] TODO
-  */
-  void initialiseDevice();
+    /**
+      The camera device's file descriptor. This specific value is set upon opening
+      the device and is used in I/O operations performed on the device, and upon
+      closing the device.
+    */
+    int fileDescriptor;
 
-  /**
-    // TODO
-  */
-  void uninitialiseDevice();
+    struct buffer *buffers;
+    unsigned int numberOfBuffers;
+    Frame frame;
+    size_t stride;
 
-  /**
-    // TODO
-  */
-  void initialiseFrame();
+    /**
+      Opens the camera device. Establishes a connection between a file and a file
+      descriptor. Creates an open file description that refers to a file and a
+      file descriptor that refers to that open file description. The file
+      descriptor is used by other I/O functions to refer to that file.
+      The file descriptor is set to the lowest file descriptor not currently open
+      for the current process. The open file description is new, and therefore the
+      file descriptor shall not share it with any other process in the system.
+      Throws a runtime error if the device cannot be identified, if it is not an
+      appropriate device, or if it cannot be opened.
+    */
+    void openDevice();
 
-  /**
-    // TODO
-  */
-  void startCapturing();
+    /**
+      Closes the camera device. All I/O progress is terminated and resources
+      associated with the file descriptor are freed. However, data format
+      parameters, current input or output, control values or other properties
+      remain unchanged.
+      Throws a runtime error if the device cannot be closed. This can only happen
+      if the file descriptor used upon closing the device is not a valid open file
+      descriptor of the device.
+    */
+    void closeDevice();
 
-  /**
-    // TODO
-  */
-  void stopCapturing();
+    /**
+      Initialises the camera device.
+      Checks the camera device's V4L2 capabilites, cropping capabilities, [...] TODO
+    */
+    void initialiseDevice();
 
-  /**
-    Checks if the camera has the necessary capabilities compatible with V4L2 to
-    be used for this application.
-    Queries the device capabilities and checks if they are compatible with the
-    V4L2 specification. Checks if the device supports the Video Capture
-    interface and the streaming I/O method. If any of these capabilities are not
-    apparent in the device, the method throws a runtime error.
-  */
-  void checkV4L2Capabilities();
+    /**
+      // TODO
+    */
+    void uninitialiseDevice();
 
-  /**
-    Checks if the camera has cropping capabilites.
-  */
-  void checkCroppingCapabilites();
+    /**
+      // TODO
+    */
+    void initialiseFrame();
 
-  /**
-    Initialises the camera device video format.
-    Sets the width and height of the image to the values passed to the class
-    constructor, sets the pixel format to YUYV and interlaces the video.
-    // TODO: Deinterlace the video if needed.
-  */
-  void initialiseFormat();
+    /**
+      // TODO
+    */
+    void startCapturing();
 
-  /**
-    // TODO
-  */
-  void initialiseBuffer();
+    /**
+      // TODO
+    */
+    void stopCapturing();
 
-  /**
-    // TODO
-  */
-  bool readFrame();
+    /**
+      Checks if the camera has the necessary capabilities compatible with V4L2 to
+      be used for this application.
+      Queries the device capabilities and checks if they are compatible with the
+      V4L2 specification. Checks if the device supports the Video Capture
+      interface and the streaming I/O method. If any of these capabilities are not
+      apparent in the device, the method throws a runtime error.
+    */
+    void checkV4L2Capabilities();
+
+    /**
+      Checks if the camera has cropping capabilites.
+    */
+    void checkCroppingCapabilites();
+
+    /**
+      Initialises the camera device video format.
+      Sets the width and height of the image to the values passed to the class
+      constructor, sets the pixel format to YUYV and interlaces the video.
+      // TODO: Deinterlace the video if needed.
+    */
+    void initialiseFormat();
+
+    /**
+      // TODO
+    */
+    void initialiseBuffer();
+
+    /**
+      // TODO
+    */
+    bool readFrame();
+
+  };
 
 };
 
