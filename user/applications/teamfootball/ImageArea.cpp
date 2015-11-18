bdc047ef97de8a4766b0b305eee7880245e497c2
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-11-18 10:32:08 +0200

    Vision: Configured blob parameters for ball detection. #108

--- user/applications/teamfootball/ImageArea.cpp
+++ user/applications/teamfootball/ImageArea.cpp
@@ -81,38 +81,44 @@ namespace rtx {
         }
         //std::cout << (*blob)->getPosition()->getX() << " " << (*blob)->getPosition()->getY() << " " << minX << " " << maxX << " " << minY << " " << maxY << std::endl;
         Color color = (*blob)->getColor();
+        double density = (*blob)->getDensity();
+        unsigned int boxArea = (*blob)->getBoxArea();
 
-        unsigned int value = 0;
-        for (unsigned int i = minX; i <= maxX; ++i) {
-          guint8 *pixel = pixels + i * channels + minY * stride;
-          for (unsigned int p = 0; p < 3; ++p) {
-            pixel[p] = value;
-          }
-          pixel = pixels + i * channels + maxY * stride;
-          for (unsigned int p = 0; p < 3; ++p) {
-            pixel[p] = value;
-          }
-        }
-        for (unsigned int j = minY; j <= maxY; ++j) {
-          guint8 *pixel = pixels + minX * channels + j * stride;
-          for (unsigned int p = 0; p < 3; ++p) {
-            pixel[p] = value;
-          }
-          pixel = pixels + maxX * channels + j * stride;
-          for (unsigned int p = 0; p < 3; ++p) {
-            pixel[p] = value;
-          }
-        }
+        if (color == BALL && density > 0.6 && boxArea > 5 * 5 && density <= 1.0 && boxArea <= CAMERA_WIDTH * CAMERA_HEIGHT) { // TODO: Remove self-explanatory checks
 
-        for (int dx = -3; dx < 3; ++dx) {
-          for (int dy = -3; dy < 3; ++dy) {
-            if (x + dx < CAMERA_WIDTH && x + dx >= 0 && y + dy < CAMERA_HEIGHT && y + dy >= 0) {
-              guint8 *pixel = pixels + (x + dx) * channels + (y + dy) * stride;
-              pixel[0] = 255;
-              pixel[1] = 0;
-              pixel[2] = 0;
+          unsigned int value = 0;
+          for (unsigned int i = minX; i <= maxX; ++i) {
+            guint8 *pixel = pixels + i * channels + minY * stride;
+            for (unsigned int p = 0; p < 3; ++p) {
+              pixel[p] = value;
+            }
+            pixel = pixels + i * channels + maxY * stride;
+            for (unsigned int p = 0; p < 3; ++p) {
+              pixel[p] = value;
             }
           }
+          for (unsigned int j = minY; j <= maxY; ++j) {
+            guint8 *pixel = pixels + minX * channels + j * stride;
+            for (unsigned int p = 0; p < 3; ++p) {
+              pixel[p] = value;
+            }
+            pixel = pixels + maxX * channels + j * stride;
+            for (unsigned int p = 0; p < 3; ++p) {
+              pixel[p] = value;
+            }
+          }
+
+          for (int dx = -3; dx < 3; ++dx) {
+            for (int dy = -3; dy < 3; ++dy) {
+              if (x + dx < CAMERA_WIDTH && x + dx >= 0 && y + dy < CAMERA_HEIGHT && y + dy >= 0) {
+                guint8 *pixel = pixels + (x + dx) * channels + (y + dy) * stride;
+                pixel[0] = 255;
+                pixel[1] = 0;
+                pixel[2] = 0;
+              }
+            }
+          }
+
         }
 
       }
