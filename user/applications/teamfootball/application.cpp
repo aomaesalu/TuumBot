edf9e3cd3d640395a6df62a3d59701a5806b629f
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-11-20 22:41:37 +0200

    Vision: Removed visioning GUI from the team football application. #132

--- user/applications/teamfootball/application.cpp
+++ user/applications/teamfootball/application.cpp
@@ -7,43 +7,13 @@
  */
 
 #include "application.hpp"
-#include "GUI.hpp"
 
 #include "tfb_logic.hpp"
 
-#include <thread>
 #include <iostream> // TODO: Remove
 
 using namespace std;
 using namespace rtx;
-
-static void run(GUI *gui) {
-  clock_t startTime = clock();
-  clock_t lastTime = startTime;
-  bool running = true;
-  while(running) {
-    rtx::hal::process();
-
-    Visioning::process();
-    Localization::process();
-    Motion::process();
-
-    Logic::process();
-
-    gui->updateFrame();
-
-    /*
-    clock_t currentTime = clock();
-    if (float(currentTime - lastTime) / CLOCKS_PER_SEC > 1) {
-      for (Visioning::BallSet::iterator ball = Visioning::balls.begin(); ball != Visioning::balls.end(); ++ball) {
-        std::cout << "Ball(d:" << (*ball)->getDistance() << ", a:" << (*ball)->getAngle() << ");" << std::endl;
-      }
-      lastTime = currentTime;
-      std::cout << std::endl << std::endl;
-    }
-    */
-  }
-}
 
 int main(int argc, char *argv[]) {
   printf("main(): Default tuum system application.\n");
@@ -58,10 +28,29 @@ int main(int argc, char *argv[]) {
 
   Logic::setup();
 
-  GUI gui(argc, argv, hal::hw.getFrontCamera());
+  clock_t startTime = clock();
+  clock_t lastTime = startTime;
+  bool running = true;
+  while(running) {
+    rtx::hal::process();
 
-  std::thread applicationThread(run, &gui);
-  applicationThread.detach();
+    Visioning::process();
+    Localization::process();
+    Motion::process();
 
-  return gui.run();
+    Logic::process();
+
+    /*
+    clock_t currentTime = clock();
+    if (float(currentTime - lastTime) / CLOCKS_PER_SEC > 1) {
+      for (Visioning::BallSet::iterator ball = Visioning::balls.begin(); ball != Visioning::balls.end(); ++ball) {
+        std::cout << "Ball(d:" << (*ball)->getDistance() << ", a:" << (*ball)->getAngle() << ");" << std::endl;
+      }
+      lastTime = currentTime;
+      std::cout << std::endl << std::endl;
+    }
+    */
+  }
+
+  return 0;
 }
