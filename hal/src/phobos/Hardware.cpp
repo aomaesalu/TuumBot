185fe94cbf63e691b5c637f7a1b67b8011fe52cb
Meelik Kiik <kiik.meelik@gmail.com>
2015-11-20 20:31:47 +0200

    Merge with branch 'hal'.

--- hal/src/phobos/Hardware.cpp
+++ hal/src/phobos/Hardware.cpp
@@ -1,8 +1,7 @@
 /** @file Hardware.cpp
  *  Platform Hardware class implementation.
  *
- *  @authors Meelik Kiik
- *  @authors Ants-Oskar Mäesalu
+ *  @authors Ants-Oskar Mäesalu, Kristjan Kanarbik, Meelik Kiik
  *  @version 0.1
  *  @date 11. November 2015
  */
@@ -18,12 +17,20 @@ namespace rtx { namespace hal {
     m_frontCamera(CAMERA_DEVICE, CAMERA_WIDTH, CAMERA_HEIGHT)//,
     //m_backCamera(CAMERA_DEVICE, CAMERA_WIDTH, CAMERA_HEIGHT) // TODO: Detect correct camera device for back camera; currently the cameras are the same.
   {
-
+    //referee = new SerialPort();
+    //referee2 = new SerialPort();
   }
 
   void Hardware::init() {
     printf("[Hardware::init]Loading hardware...\n");
     m_motorControl.init();
+
+    //refereeListener.init("/dev/ttyACM0");
+    //refereeListener2.init("/dev/ttyACM1");
+    //refereeListener.write_some("aAXASTART----");
+    //referee->init("/dev/ttyACM0", 9600);
+    //referee2->init("/dev/ttyACM1", 9600);
+    //referee->write_some("aABKICKOFF----");
   }
 
   Camera* Hardware::getFrontCamera() {
@@ -31,7 +38,7 @@ namespace rtx { namespace hal {
   }
 
   Camera* Hardware::getBackCamera() {
-    return nullptr;//&m_backCamera;
+    return nullptr; //&m_backCamera;
   }
 
   MotorControl* Hardware::getMotorControl() {
---
+++ hal/src/phobos/RefereeListener.hpp
@@ -0,0 +1,95 @@
+#ifndef RTX_REF_H
+#define RTX_REF_H
+
+#include <functional>
+#include <string>
+#include <map>
+
+#include "SerialPort.hpp"
+
+namespace rtx { namespace hal {
+
+  enum RefereeSignal {
+    REF_VOID,
+    REF_START,
+    REF_STOP,
+    REF_PLACEDBAL,
+    REF_ENDHALF,
+    REF_KICKOFF,
+    REF_IFREEK,
+    REF_DFREEK,
+    REF_GOALK,
+    REF_THROWIN,
+    REF_CORNERK,
+    REF_PENALTY,
+    REF_GOAL,
+    REF_CARDY,
+
+  };
+
+  typedef std::map<std::string, RefereeSignal> RefSigMap;
+
+  const RefSigMap refSigMap = {
+    {"START----", REF_START},
+    {"STOP-----", REF_STOP},
+    {"PLACEDBAL", REF_PLACEDBAL},
+    {"ENDHALF--", REF_ENDHALF},
+    {"KICKOFF-", REF_KICKOFF},
+    {"IFREEK--", REF_IFREEK},
+    {"DFREEK--", REF_DFREEK},
+    {"GOALK---", REF_GOALK},
+    {"THROWIN-", REF_THROWIN},
+    {"CORNERK-", REF_CORNERK},
+    {"PENALTY-", REF_PENALTY},
+    {"GOAL---- ", REF_GOAL},
+    {"CARDY---", REF_CARDY}
+
+  };
+
+  // Field, Robot ID, Team
+  struct RefTarget {
+    char field;
+    char team;
+    char id;
+  };
+
+  struct RefCommand {
+    RefereeSignal sig;
+    RefTarget target;
+  };
+
+  typedef void (*VoidFn)(RefCommand);
+  typedef std::pair<RefereeSignal, VoidFn> SignalCallback;
+
+  inline void test(RefereeSignal s) {
+
+  }
+
+  //FIXME: This is not regularly working over different threads.
+  static std::map<RefereeSignal, VoidFn> callbacks;
+
+  class RefereeListener : public SerialPort {
+  private:
+
+  public:
+    RefereeListener();
+
+    void init(std::string portname);
+
+    void on_receive_(const std::string &data);
+
+    void registerCallback(const RefereeSignal, VoidFn);
+    void signal(RefCommand);
+
+    RefCommand parseCommand(std::string);
+
+    void sendAck();
+
+    const char FIELD = 'A';
+    const char ID = 'B';
+    const char TEAM = 'A';
+  };
+
+}}
+
+#endif // RTX_REF_H
