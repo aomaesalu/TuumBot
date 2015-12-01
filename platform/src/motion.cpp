86ddacfd6f6cf66939f065ac0f05be2e1c14facc
Meelik Kiik <kiik.meelik@gmail.com>
2015-12-01 21:03:09 +0200

    Tweak motion constants.

--- platform/src/motion.cpp
+++ platform/src/motion.cpp
@@ -4,11 +4,11 @@
 
 namespace rtx { namespace Motion {
 
-  const ValueLevels<int> VLS_DIST = {5000, 850, 700, 350, 200};
+  const ValueLevels<int> VLS_DIST = {5000, 1000, 600, 300, 200};
   const ValueLevels<double> VLS_ANGLE = {2*3.14, 0.38, 0.27, 0.20, 0.10};
 
   //TODO: Movement gears
-  const Gears GRS_MOV = {{110, 275}, {80, 175}, {40, 100}, {35, 50}, {30, 50}};
-  const Gears GRS_ROT = {{0, 0.3}, {1, 0.30}, {10, 0.20}, {12, 0.05}, {20, 0.4}};
+  const Gears GRS_MOV = {{100, 200}, {60, 175}, {40, 100}, {35, 50}, {30, 50}};
+  const Gears GRS_ROT = {{5, 0.25}, {5, 0.25}, {10, 0.15}, {15, 0.05}, {12, 0.08}};
 
 }}
