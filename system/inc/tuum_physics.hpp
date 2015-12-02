ac6776faafb848856b3c75be2337d153b9a64fc7
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-12-03 00:47:54 +0200

    Physics: Initialised physics system. #226

---
+++ system/inc/tuum_physics.hpp
@@ -0,0 +1,19 @@
+/**
+ *  @file tuum_physics.hpp
+ *  Physics system interface
+ *
+ *  @authors Ants-Oskar Mäesalu
+ *  @version 0.1
+ *  @date 3 December 2015
+*/
+
+#ifndef RTX_SYSTEM_PHYSICS_H
+#define RTX_SYSTEM_PHYSICS_H
+
+namespace rtx { namespace Physics {
+
+  void rayCast();
+
+}}
+
+#endif // RTX_SYSTEM_PHYSICS_H
---
+++ system/src/tuum_physics.cpp
@@ -0,0 +1,21 @@
+/**
+ *  @file tuum_physics.hpp
+ *  Physics system interface
+ *
+ *  @authors Ants-Oskar Mäesalu
+ *  @version 0.1
+ *  @date 3 December 2015
+*/
+
+#include "tuum_physics.hpp"
+
+#include "tuum_vision.hpp"
+#include "tuum_visioning.hpp"
+
+namespace rtx { namespace Physics {
+
+  void rayCast() {
+    // TODO
+  }
+
+}}
