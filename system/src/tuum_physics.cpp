e514dcfd259549df119f0dfaa3c69226427af682
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-12-03 01:36:58 +0200

    Vision: Implemented line ray casting angle calculation. #210

--- system/src/tuum_physics.cpp
+++ system/src/tuum_physics.cpp
@@ -23,21 +23,15 @@ namespace rtx { namespace Physics {
     BallSet balls = *(Visioning::ballDetect().getEntities());
     for (BallSet::iterator ball = balls.begin(); ball != balls.end(); ++ball) {
 
-      // Get blob constants
-      unsigned int minX = (*ball)->getBlob()->getMinX();
-      unsigned int maxX = (*ball)->getBlob()->getMaxX();
-      unsigned int minY = (*ball)->getBlob()->getMinY();
-      unsigned int maxY = (*ball)->getBlob()->getMaxY();
-
       // If the angle is positive, the ray is located to the left from the
       // center of the camera frame
       if (angle >= 0) {
 
-        // TODO: Check for bottom left and top right corner
-
         // Calculate corresponding blob corner angles
-        double bottomLeftAngle = /* TODO */;
-        double topRightAngle = /* TODO */;
+        double bottomLeftAngle = atan2((*ball)->getBlob()->getMinX(),
+                                       (*ball)->getBlob()->getMaxY());
+        double topRightAngle = atan2((*ball)->getBlob()->getMaxX(),
+                                     (*ball)->getBlob()->getMinY());
 
         // If the angle is smaller than the bottom left corner's angle and
         // larger than the top right corner's angle, the blob is in the way of
@@ -53,18 +47,18 @@ namespace rtx { namespace Physics {
       // center of the camera frame
       } else {
 
-        // TODO: Check for top left and bottom right corner
-
         // Calculate corresponding blob corner angles
-        double topLeftAngle = /* TODO */;
-        double bottomRightAngle = /* TODO */;
+        double topLeftAngle = atan2((*ball)->getBlob()->getMinX(),
+                                    (*ball)->getBlob()->getMinY());
+        double bottomRightAngle = atan2((*ball)->getBlob()->getMaxX(),
+                                        (*ball)->getBlob()->getMaxY());
 
         // If the angle is smaller than the top left corner's angle and larger
         // than the bottom right corner's angle, the blob is in the way of the
         // ray.
         if (angle <= topLeftAngle && angle >= bottomRightAngle) {
 
-          // TODO: If the blob is closer than the closes object, add it to the
+          // TODO: If the blob is closer than the closest object, add it to the
           // result
 
         }
