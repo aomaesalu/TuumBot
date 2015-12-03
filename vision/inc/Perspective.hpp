8ed578c9fed1b7502441bd5cf101224ac7b4a934
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2015-12-03 13:05:50 +0200

    Vision: Added partial point class usage to the perspective coordinate transformation methods. #229

--- vision/inc/Perspective.hpp
+++ vision/inc/Perspective.hpp
@@ -4,11 +4,13 @@
  *
  *  @authors Ants-Oskar Mäesalu
  *  @version 0.1
- *  @date 29 November 2015
+ *  @date 3 December 2015
  */
 
 #ifndef RTX_VISION_PERSPECTIVE_H
 #define RTX_VISION_PERSPECTIVE_H
+
+#include "Point2D.hpp"
 
 #include <utility>
 
@@ -21,8 +23,12 @@ namespace rtx { namespace Vision { namespace Perspective {
 
   // TODO: Maybe move samples here?
 
+  // TODO: Use point class
+
   std::pair<double, double> virtualToReal(const unsigned int&, const unsigned int&);
   std::pair<double, double> virtualToReal(const std::pair<unsigned int, unsigned int>&);
+  std::pair<double, double> virtualToReal(const Point2D*);
+
   std::pair<unsigned int, unsigned int> realToVirtual(const double&, const double&);
   std::pair<unsigned int, unsigned int> realToVirtual(const std::pair<double, double>&);
 
