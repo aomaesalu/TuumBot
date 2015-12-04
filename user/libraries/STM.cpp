b2a74da08550101e4fc144b5fce2c066d4dd686e
Meelik Kiik <kiik.meelik@gmail.com>
2015-12-04 22:54:47 +0200

    Implement 'EventEmitter' in STM module and derive STM from it.

--- user/libraries/STM.cpp
+++ user/libraries/STM.cpp
@@ -23,37 +23,17 @@ namespace rtx {
     m_priority = State::priority_seq++;
   }
 
-  STM::STM() {
-    m_state = nullptr;
-  }
-
-  void STM::setState(State* st) {
-    std::cout << "[STM]Transition: ";
-    if(m_state != nullptr) {
-      std::cout << m_state->getName() << " => ";
-    }
-    std::cout << st->getName() << std::endl;
-
-    m_state = st;
-    st->setup();
-  }
-
-  void STM::addRootState(State* st) {
-    m_rootStates.push_back(st);
-  }
-
-  State* STM::createState(std::string name) {
-    State* st = new State(name, this);
-    m_states.push_back(st);
-    return st;
-  }
-
   State* State::getLastState() {
     return m_last;
   }
 
   State* State::getNextState() {
     return m_next;
+  }
+
+  void State::addController(Controller* ctrl) {
+    m_controllers.push_back(ctrl);
+    ctrl->onStateAttach(this, m_stm);
   }
 
   bool State::canEnter() {
@@ -82,13 +62,48 @@ namespace rtx {
     }
   }
 
+  /**
+   *  State Machine methods implementation.
+   */
+  STM::STM():
+    EventEmitter()
+  {
+    m_state = nullptr;
+  }
+
+  void STM::setState(State* st) {
+    std::cout << "[STM]Transition: ";
+    if(m_state != nullptr) {
+      std::cout << m_state->getName() << " => ";
+    }
+    std::cout << st->getName() << std::endl;
+
+    m_state = st;
+    st->setup();
+  }
+
+  void STM::addRootState(State* st) {
+    m_rootStates.push_back(st);
+  }
+
+  State* STM::createState(std::string name) {
+    State* st = new State(name, this);
+    m_states.push_back(st);
+    return st;
+  }
+
+  void STM::setup() {
+    EventEmitter::init();
+    //if(m_states.size() > 0) setState(m_states[0]);
+  }
+
   void STM::process() {
     // Process root states
     for(auto& tmp_st_ptr : m_rootStates) {
       if(tmp_st_ptr->getPriority() < m_state->getPriority()) continue;
       if(tmp_st_ptr->canEnter() && tmp_st_ptr != m_state) {
         setState(tmp_st_ptr);
-	return;
+        return;
       }
     }
 
@@ -120,4 +135,52 @@ namespace rtx {
 
     return;
   }
+
+
+  /**
+   *  Event emitter methods implementation.
+   *  TODO: refactor code into separate library
+   */
+  void EventEmitter::init() {
+    m_eventMap.clear();
+    m_eventListeners.clear();
+    m_EventListenerIDSeq = 1;
+  }
+
+  EventID EventEmitter::registerEvent(EventName evn) {
+    m_eventMap[evn] = m_EventListenerIDSeq++;
+  }
+
+  EventID EventEmitter::getEventID(EventName evn) {
+    auto it = m_eventMap.find(evn);
+    if(it != m_eventMap.end()) return it->second;
+    return registerEvent(evn);
+  }
+
+  EventHandler EventEmitter::getEventHandler(EventID id) {
+    auto it = m_eventListeners.find(id);
+    if(it != m_eventListeners.end()) return it->second;
+    throw(-1);
+  }
+
+  void EventEmitter::registerEventListener(EventID id, EventHandler evh) {
+    if(id == 0) return;
+    m_eventListeners[id] = evh;
+  }
+
+  void EventEmitter::emitEvent(EventName evn) {
+    EventID id = getEventID(evn);
+    if(id == 0) return;
+
+    EventHandler evh;
+    try { evh = getEventHandler(id); }
+    catch (int err) {
+      std::cout << "[EventEmitter]Error " << err
+                << ": Unknown handler id " << id << std::endl;
+      return;
+    }
+
+    evh();
+  }
+
 }
--- user/libraries/STM.hpp
+++ user/libraries/STM.hpp
@@ -10,11 +10,36 @@
 #define RTX_STM_H
 
 #include <vector>
+#include <map>
 #include <functional>
 #include <string>
 #include <iostream>
+#include <stdint.h>
 
+//TODO: Implement separate dedicated event emitter base class
 namespace rtx {
+
+  typedef uint32_t EventID;
+  typedef std::string EventName;
+  typedef void (*EventHandler)();
+
+  class EventEmitter {
+  protected:
+    std::map<EventName, uint32_t> m_eventMap;
+    std::map<uint32_t, EventHandler> m_eventListeners;
+
+    EventID m_EventListenerIDSeq;
+  public:
+    void init();
+
+    EventID registerEvent(EventName);
+    EventID getEventID(EventName);
+
+    EventHandler getEventHandler(EventID);
+
+    void registerEventListener(EventID, EventHandler);
+    void emitEvent(EventName);
+  };
 
   class STM;
   class State;
@@ -28,6 +53,7 @@ namespace rtx {
   typedef std::vector<Controller*> CtrlSet;
   typedef std::vector<State*> StateSet;
 
+
   enum CtrlPhase {
     CP_INIT,
     CP_RUN,
@@ -37,14 +63,6 @@ namespace rtx {
   struct Context {
     CtrlPhase phase = CP_INIT;
     State* st;
-  };
-
-  class Controller {
-  public:
-    virtual int run() {}
-    virtual void init() {}
-    virtual bool isRunnable() { return true; }
-    virtual bool isInterruptable() { return true; }
   };
 
   class State {
@@ -64,15 +82,12 @@ namespace rtx {
     State(std::string name, STM* stm);
 
     std::string getName() { return m_name; }
-
-    void addController(Controller* ctrl) {
-      m_controllers.push_back(ctrl);
-    }
-
     int getPriority() { return m_priority; }
 
     void setLastState(State* st) { m_last = st; }
     void setNextState(State* st) { m_next = st; }
+
+    void addController(Controller*);
 
     State* getLastState();
     State* getNextState();
@@ -84,7 +99,7 @@ namespace rtx {
     void process();
   };
 
-  class STM {
+  class STM : public EventEmitter {
   public:
     STM();
 
@@ -95,12 +110,42 @@ namespace rtx {
 
     State* createState(std::string);
 
+    void setup();
     void process();
   private:
     StateSet m_states;
     StateSet m_rootStates;
 
     State* m_state;
+  };
+
+
+  class Controller {
+  protected:
+    State* m_st = nullptr;
+    STM* m_stm = nullptr;
+
+  public:
+    virtual int run() {}
+    virtual void init() {}
+    virtual bool isRunnable() { return true; }
+    virtual bool isInterruptable() { return true; }
+
+    //m_stm->registerEvent("DONE")
+    //m_stm->emitEvent("DONE")
+    //m_stm->addEventListener(EventName, EventHandler)
+    virtual void onStateAttach(State* st, STM* stm) {
+      m_st = st;
+      m_stm = stm;
+    };
+
+    virtual void emit(EventName evn) {
+      if(m_stm != nullptr) m_stm->emitEvent(evn);
+    }
+
+    virtual void registerEvent(EventName evn) {
+      if(m_stm != nullptr) m_stm->registerEvent(evn);
+    }
   };
 
 }
