25add593a953a960439e15251dd98ccde5e48075
Kristjan Kanarbik <kristjan.k@hotmail.com>
2015-12-05 23:41:32 +0200

    Minor changes to Smacktalk

--- user/applications/soundtest/application.cpp
+++ user/applications/soundtest/application.cpp
@@ -5,19 +5,18 @@ using namespace std;
 
 int main(){
   printf("Soundtest\n");
-  rtx::Smacktalk::SmackTalk *smacktalk = new rtx::Smacktalk::SmackTalk();
-  smacktalk->init();
+  rtx::Smacktalk::init();
 
   bool running = true;
   while(running) {
     usleep(1000000);
     printf("Calling goalHit!");
     clock_t begin = clock();
-    smacktalk->goalHit();
+    rtx::Smacktalk::goalHit();
 
     clock_t end = clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-    printf("Time elapsed: %G", elapsed_secs);
+    printf("Time elapsed: %f\n\n\n", elapsed_secs);
   }
 
   return 0;
