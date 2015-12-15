30d26ad3fa6db304eec40b2945e05a9b43b0a58e
Meelik Kiik <kiik.meelik@gmail.com>
2015-12-15 14:37:14 +0200

    Update gui test program.

--- user/applications/tests/gui/main.cpp
+++ user/applications/tests/gui/main.cpp
@@ -1,7 +1,29 @@
 
+#include <boost/thread.hpp>
+#include <boost/date_time.hpp>
+
 #include "rtxgui.hpp"
 
+#include "application.hpp"
+
+using namespace rtx;
+
+void sys_init() {
+  rtx::hal::setup();
+  Visioning::setup();
+}
+
+void sys_main() {
+  while(1) {
+    rtx::hal::process();
+    Visioning::process();
+  }
+}
 
 int main(int argc, char *argv[]) {
+  rtx::init(argc, argv);
+  sys_init();
+  boost::thread workerThread(sys_main);
+
   return rtx::gui::run(argc, argv);
 }
